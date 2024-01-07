5ec61ecea70975232b35a45cacd006a609c05635
Waleed <habibking541@gmail.com>
2024-01-06 22:02:06 -0800

    waleed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
-app.use(express.static("template"));
+const path = require("path");
+const templatePath = path.join(__dirname,"template")
+app.use(express.static(templatePath));
 app.get("/",(req,res) => {
     res.render("index")
 });
+app.get("/check",(req,res) => {
+    res.send("hello");
+})
 app.listen(8000,() => {
     console.log("i am running")
 })
