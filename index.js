f1f00511cb1735e400145d9840bf0ecd124be281
Waleed <habibking541@gmail.com>
2024-01-06 22:44:32 -0800

    Waleed

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const templatePath = path.join(__dirname,"template")
-app.use(express.static(templatePath));
-app.get("/",(req,res) => {
-    res.render("index")
+const hbs = require("hbs");
+const templatePath = path.join(__dirname,"template/views");
+const itemPath = path.join(__dirname,"template/items")
+app.set('view engine','hbs');
+app.set('views',templatePath);
+app.use(express.static(itemPath))
+app.get("/", async(req,res) => {
+    try{
+        res.render("index");
+    }catch(err){
+        console.log(err);
+    }
+    
 });
+app.get("/login",(req,res) => {
+    res.render("login")
+})
 app.get("/check",(req,res) => {
     res.send("hello");
 })
